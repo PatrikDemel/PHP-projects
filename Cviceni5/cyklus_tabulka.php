17db44f8c061fcceffbe1d40516fc4eaff38feaa
PatrikDemel <d20133@oaopava.cz>
2023-11-02 22:48:29 +0100

    finished

---
+++ Cviceni5/cyklus_tabulka.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * cyklus
+ * 1. Vytvořte tabulku, která bude obsahovat tři sloupce a n řádků. Obsah tabulky bude tvořen obsahem pole $city. Obsah každé buňky bude ve tvaru Pořadové číslo. Stát - Hlavní město
+ * Příklad výstupu:
+ * | Stát - hlavní město | Stát - hlavní město 		| Stát - hlavní město  |
+ * |1. Italy - Rome 	 |2. Luxembourg - Luxembourg    |3. Belgium - Brussels |
+ *
+ * 2. Tabulku setřiďte vzestupně dle názvu státu (A-Z)
+ *
+ */
+
+$city = array(
+	"Italy" => "Rome",
+	"Luxembourg" => "Luxembourg",
+	"Belgium" => "Brussels",
+	"Denmark" => "Copenhagen",
+	"Finland" => "Helsinki",
+	"France" => "Paris",
+	"Slovakia" => "Bratislava",
+	"Slovenia" => "Ljubljana",
+	"Germany" => "Berlin",
+	"Greece" => "Athens",
+	"Ireland" => "Dublin",
+	"Netherlands" => "Amsterdam",
+	"Portugal" => "Lisbon",
+	"Spain" => "Madrid",
+	"Sweden" => "Stockholm",
+	"United Kingdom" => "London",
+	"Cyprus" => "Nicosia",
+	"Lithuania" => "Vilnius",
+	"Czech Republic" => "Prague",
+	"Estonia" => "Tallin",
+	"Hungary" => "Budapest",
+	"Latvia" => "Riga",
+	"Malta" => "Valetta",
+	"Austria" => "Vienna",
+	"Poland" => "Warsaw"
+);
+ksort($city);
+
+echo '<table>';
+$colnum = 0;
+foreach ($city as $country => $capital) {
+	if ($colnum % 3 == 0) {
+		echo '<tr>';
+	}
+	echo "<td>$country - $capital</td>";
+	$colnum++;
+
+	if ($colnum % 3 == 0) {
+		echo '</tr>';
+	}
+}
+echo '</table>';
+?>
---
+++ Cviceni5/pole_mesic.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Asociativní pole
+ * Deklarujte pole months, které bude mít v hodnotách uložen český název měsíce. Index bude odpovídat pořadovému číslu měsíce.
+ */
+
+//reseni
+
+$months = array(null, 'Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen', 'Červenec', 'Srpen', 'Září', 'Říjen', 'Listopad', 'Prosinec');
+
+?>
